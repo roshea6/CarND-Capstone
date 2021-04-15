93bea871988c9a8e537b3b938e580699dd919fa3
roshea6 <rpo1202@gmail.com>
2021-04-15 01:17:26 +0000

    Working classifier training. Added lambda normalization layer at beginning of network.

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,4 +1,4 @@
-# from styx_msgs.msg import TrafficLight
+from styx_msgs.msg import TrafficLight
 
 # import tensorflow as tf 
 import cv2, glob
@@ -25,9 +25,11 @@ class TLClassifier(object):
         self.yellow_path = self.save_path + 'yellow/'
         self.red_path = self.save_path + 'red/'
         
-        self.img_shape = None
+        self.img_shape = (600, 800, 3)
         
-        self.save_file = "/home/workspace/CarND-Capstone/ros/src/tl_detector/light_classification/models/test.h5"
+        self.save_file = "./models/newest_model.h5"
+        
+        self.current_best_model = "./models/best_model.h5"
     
     def loadData(self):
         training_imgs = []
@@ -36,30 +38,21 @@ class TLClassifier(object):
         # Loop through the different data folders and append the img and label to the lists
         for img_name in glob.glob(self.green_path + '*.jpg'):
             img = cv2.imread(img_name, cv2.IMREAD_COLOR)
-            if not img.shape == (600, 800, 3):
-                print "Bad"
             training_imgs.append(img)
             training_labels.append(0)
             
         for img_name in glob.glob(self.yellow_path + '*.jpg'):
             img = cv2.imread(img_name, cv2.IMREAD_COLOR)
-            if not img.shape == (600, 800, 3):
-                print "Bad"
             training_imgs.append(img)
             training_labels.append(1)
             
         for img_name in glob.glob(self.red_path + '*.jpg'):
             img = cv2.imread(img_name, cv2.IMREAD_COLOR)
-            if not img.shape == (600, 800, 3):
-                print "Bad"
             training_imgs.append(img)
             training_labels.append(2)
          
         if len(training_imgs) > 0:
             self.img_shape = training_imgs[0].shape
-            print(self.img_shape)
-            #print(len(training_imgs))
-            #print(training_imgs[0])
             
         return np.asarray(training_imgs), np.asarray(training_labels)
     
@@ -77,24 +70,17 @@ class TLClassifier(object):
         # TODO: Define our neural net, optimizer, split into training and testing data, and train the network
         # Define our CNN
         model = Sequential()
+        
+        # Takes in a 600x800x3 image and normalize it
+        model.add(Lambda(lambda x: (x/255.0) - 0.5, input_shape = self.img_shape))
 
-        # Takes in a 600x800x3 image
-        model.add(Convolution2D(32, 3, 3, subsample=(2,2), input_shape = self.img_shape, activation = 'relu'))
-        # model.add(Convolution2D(32, 3, 3, subsample=(2,2), activation = 'relu'))
+        model.add(Convolution2D(32, 3, 3, subsample=(2,2), activation = 'relu'))
 
         model.add(MaxPooling2D((2,2)))
 
         model.add(Convolution2D(64, 3, 3, subsample=(2,2), activation = 'relu'))
-        # model.add(Convolution2D(64, 3, 3, subsample=(2,2), activation = 'relu'))
-        #model.add(Conv2D(64, (3,3), activation = 'relu'))
 
         model.add(MaxPooling2D((2,2)))
-
-        # model.add(Convolution2D(128, 3, 3, subsample=(2,2), activation = 'relu'))
-        # model.add(Convolution2D(128, 3, 3, subsample=(2,2), activation = 'relu'))
-        #model.add(Conv2D(128, (3,3), activation = 'relu'))
-
-        # model.add(MaxPooling2D((2,2)))
 
         model.add(Flatten())
 
@@ -104,67 +90,60 @@ class TLClassifier(object):
 
         model.add(Dense(3, activation = 'softmax'))
 
-        # Convolution layer 1 with 24 5x5 filters
-        # model.add(Convolution2D(24, 5, 5, subsample=(2,2), input_shape = self.img_shape, activation='relu'))
-        # # model.add(Dropout(.2))
-
-        # # Convolution layer 2 with 36 5x5 filters
-        # model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation='relu'))
-
-        # # Convolution layer 3 with 48 5x5 filters
-        # model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation='relu'))
-
-        # # Convolution layer 4 with 64 3x3 filters
-        # model.add(Convolution2D(64, 3, 3, activation='relu'))
-
-        # # Convolution layer 5 with 64 3x3 filters
-        # model.add(Convolution2D(64, 3, 3, activation='relu'))
-
-        # # Flatten the output of the last convolution layer so it can be connected with a fully connected layer
-        # model.add(Flatten())
-
-        # # Fully Connected layer 1
-        # model.add(Dense(100))
-        # model.add(Dropout(.50))
-
-        # # Fully Connected layer 2
-        # model.add(Dense(50))
-        # model.add(Dropout(.50))
-
-        # # Fully Connected layer 2
-        # model.add(Dense(10))
-
-        # # Output single node (The steering angle)
-        # model.add(Dense(3, activation = 'softmax'))
-        
-        # Split into training and validation data
-        # img_train, img_test, label_train, label_test = train_test_split(imgs, labels, test_size = 0.20, random_state = 7, shuffle = True)
-
         # Compile the model with the optimizer, loss function, and desired metric
         model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
         # Callback checkpoint to save the model's weights whenever it improves over the best
-        checkpoint = ModelCheckpoint("test.h5", monitor='accuracy', verbose=1, save_best_only=True, mode='auto', period=1)
+        checkpoint = ModelCheckpoint(self.save_file, monitor='accuracy', verbose=1, save_best_only=True, mode='auto', period=1)
 
         # Train the model with our data 
-        model.fit(x = imgs, y = labels, epochs = 10, validation_split=.2, callbacks=[checkpoint])
+        model.fit(x = imgs, y = labels, epochs = 4, validation_split=.2, callbacks=[checkpoint])
 
-        model.save("test.h5")
+        model.save(self.save_file)
 
         # self.evaluateModel(model, img_train, label_train)
+        
+    # Load and returns the current best model
+    def loadModel(self):
+        print(f"Loading model from {self.current_best_model}")
 
-    # def get_classification(self, image):
-    #     """Determines the color of the traffic light in the image
+        model = load_model(self.current_best_model)
 
-    #     Args:
-    #         image (cv::Mat): image containing the traffic light
+        # Shape the image to the desired size
+        
 
-    #     Returns:
-    #         int: ID of traffic light color (specified in styx_msgs/TrafficLight)
+        return model
+        
 
-    #     """
-    #     #TODO implement light color prediction
-    #     return TrafficLight.UNKNOWN
+    def get_classification(self, image):
+        """Determines the color of the traffic light in the image
+
+        Args:
+            image (cv::Mat): image containing the traffic light
+
+        Returns:
+            int: ID of traffic light color (specified in styx_msgs/TrafficLight)
+
+        """
+        #TODO implement light color prediction
+        # Load the model
+        model = self.loadModel()
+        
+        # Resize the image to the size the model takes
+        image = cv2.resize(image, self.image_shape)
+        
+        # Get the output of the neural net
+        pred = model.predict(image)
+        
+        # Order the output from highest to lowest
+        pred_class = pred.argmax(axis=-1)
+        
+        # Get the actual classification
+        best_class = pred_class[0]
+        
+        print best_class
+        
+        # return TrafficLight.UNKNOWN
     
 # Use main function to test data loading and training
 if __name__ == '__main__':
@@ -173,5 +152,9 @@ if __name__ == '__main__':
 #     imgs, labels = classifier.loadData()
     
 #     print imgs.shape
+
+    img = cv2.imread("./data/green/0.jpg")
     
-    classifier.trainModel()
+    classifier.get_classification(img)
+    
+    
